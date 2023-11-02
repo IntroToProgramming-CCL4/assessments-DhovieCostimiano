8f43d80133260d228b2f97777b8ec928ad71304d
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-02 21:47:11 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 5- Dictionaries/Person Ex1 Chap5.py
@@ -0,0 +1,2 @@
+dad= {'first name': 'Sixto', 'last name': 'Costimiano', 'age':'34', 'city':'Dubai'}
+print (dad)
---
+++ Assessment 1/Chapter 5- Dictionaries/Pets Ex5 Chap5.py
@@ -0,0 +1,8 @@
+owner1= {'Pet name':'Coco', 'Owner name': 'Maria'}
+owner2= {'Pet name':'Browny', 'Owner name': 'Dhovie'}
+owner3= {'Pet name':'Zeus', 'Owner name': 'Bryan'}
+owner4= {'Pet name':'Skoby', 'Owner name': 'Yanna'}
+owner5= {'Pet name':'Nido', 'Owner name': 'Racelin'}
+pets=[owner1, owner2, owner3, owner4, owner5]
+for owner in pets:
+    print (owner)
