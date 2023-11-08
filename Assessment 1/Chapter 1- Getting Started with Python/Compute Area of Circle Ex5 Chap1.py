388ab343f2403cc631186797be34ccb5b9e91887
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:13:40 +0400

    Update Compute Area of Circle Ex5 Chap1.py

--- Assessment 1/Chapter 1- Getting Started with Python/Compute Area of Circle Ex5 Chap1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Compute Area of Circle Ex5 Chap1.py
@@ -1,4 +1,6 @@
 pi=3.14
 radius=float(input("Enter radius of the circle:"))
+#float is for numbers in python
 area=pi* (radius**2)
+#multiplies the radius
 print ("The area of the circle is:", (area))
