f1d4c7d71fbb491b423191c180f0fa21cee27ce3
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:20:51 +0400

    Update Alien Color #1 Ex1 Chap4.py

--- Assessment 1/Chapter 4- Control Flow/Alien Color #1 Ex1 Chap4.py
+++ Assessment 1/Chapter 4- Control Flow/Alien Color #1 Ex1 Chap4.py
@@ -1,9 +1,13 @@
 #Version 1
 alien_color= 'green'
 if alien_color == 'green':
+#if statements only work if certain conditions are met
     print("You just earned 5 points")
+#the alien_color is equal to green so the player gets 5 points
 
 #Version 2
 alien_color= 'yellow'
 if alien_color == 'green':
-    print("You just earned 5 points")
+#if statements only work if certain conditions are met
+    print("You just earned 10 points")
+#the alien_color is not equal to green so the player gets 10 points
