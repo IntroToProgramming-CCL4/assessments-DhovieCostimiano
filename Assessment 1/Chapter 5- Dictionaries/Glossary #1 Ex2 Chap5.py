22843bb488dfe5666b7739423a77c71934a698bc
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:23:22 +0400

    Update Glossary #1 Ex2 Chap5.py

--- Assessment 1/Chapter 5- Dictionaries/Glossary #1 Ex2 Chap5.py
+++ Assessment 1/Chapter 5- Dictionaries/Glossary #1 Ex2 Chap5.py
@@ -1,6 +1,6 @@
 glossary= {'loops':'Loops are used when we want to execute code statements multiple times.', 'comments': 'These describe what is going on inside a program', 'operators':'Operators are special symbols in Python that perform arithmetic or logicl coputaion', 'lists': 'A list is a ordered set of values where each value is identified by an index', 'functions': 'Functions are named blocks of code that are designed to do one specific job'}
 #longer way of writing this code
-#
+#You can acess a dictionary by calling out it key and value
 print ('\nProgramming word: loops',  '\nDefinition:', glossary['loops'],)
 print ('\nProgramming word: comments',  '\nDefinition:', glossary['comments'],)
 print ('\nProgramming word: operators',  '\nDefinition:', glossary['operators'],)
