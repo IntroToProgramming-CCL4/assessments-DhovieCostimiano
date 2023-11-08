df3f473ba475713c07111050fb76f38b262730d9
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:06:16 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Chapter 4.py/Alien Color #3 Ex3 Chap4.py
@@ -0,0 +1,26 @@
+#if alien_color is green the player gets 5 points
+alien_color= 'green'
+if alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color=='yellow':
+    print("You just earned 10 points")
+elif alien_color=='red':
+    print("You just earned 15 points")
+
+#if alien_color is yellow the player gets 10 points
+alien_color= 'yellow'
+if alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color=='yellow':
+    print("You just earned 10 points")
+elif alien_color=='red':
+    print("You just earned 15 points")
+
+#if alien_color is red the player gets 15 points
+alien_color= 'red'
+if alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color=='yellow':
+    print("You just earned 10 points")
+elif alien_color=='red':
+    print("You just earned 15 points")
---
+++ Assessment 1/Chapter 4- Control Flow/Chapter 4.py/Favorite Friut Ex5 Chap4.py
@@ -0,0 +1,13 @@
+favorite_fruits=['banana','guava','mandarin','dragon fruit','grapes']
+if 'banana' in favorite_fruits:
+    print('You really like bananas!')
+if 'guava' in favorite_fruits:
+    print('You really like guava!')
+if 'mandarin' in favorite_fruits:
+    print('You really like mandarin!')
+if 'dragon fruit' in favorite_fruits:
+    print('You really like dragon fruit!')
+if 'grapes' in favorite_fruits:
+    print('You really like grapes!')
+
+    
