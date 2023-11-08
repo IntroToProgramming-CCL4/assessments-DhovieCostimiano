2624e2f3b92bed27288837908707bb676629d5f8
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:30:52 +0400

    Update Infinity Ex3 Chap6.py

--- Assessment 1/Chapter 6- Loops/Exercises/Infinity Ex3 Chap6.py
+++ Assessment 1/Chapter 6- Loops/Exercises/Infinity Ex3 Chap6.py
@@ -1,4 +1,6 @@
 word = input('Enter word:')
 while True:
+#uses a while loop to perform a infinite loop
     if word == word:
         print (word)
+#prints the input word if input word is equal to input word
