1c364aabe0d508441751dba9bb8aa4f80cb20d97
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:29:27 +0400

    Update Pets Ex5 Chap5.py

--- Assessment 1/Chapter 5- Dictionaries/Pets Ex5 Chap5.py
+++ Assessment 1/Chapter 5- Dictionaries/Pets Ex5 Chap5.py
@@ -1,8 +1,9 @@
-owner1= {'Pet name':'Coco', 'Owner name': 'Maria'}
-owner2= {'Pet name':'Browny', 'Owner name': 'Dhovie'}
-owner3= {'Pet name':'Zeus', 'Owner name': 'Bryan'}
-owner4= {'Pet name':'Skoby', 'Owner name': 'Yanna'}
-owner5= {'Pet name':'Nido', 'Owner name': 'Racelin'}
+owner1= {'Animal':'Dog', 'Pet name':'Coco', 'Owner name': 'Maria'}
+owner2= {'Animal':'Cat', 'Pet name':'Browny', 'Owner name': 'Maria'}
+owner3= {'Animal':'Fish', 'Pet name':'Fishy', 'Owner name': 'Maria'}
+owner4= {'Animal':'Dragon', 'Pet name':'Dragony', 'Owner name': 'Maria'}
+owner5= {'Animal':'Unicorn', 'Pet name':'Unicorny', 'Owner name': 'Maria'}
+
 pets=[owner1, owner2, owner3, owner4, owner5]
 #you can combine all dictionaries in a list
 for owner in pets:
