2fa1ea101101872c41c05980f59cc878bb76c200
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:30:31 +0400

    Update Deli Ex4 Chap6.py

--- Assessment 1/Chapter 6- Loops/Exercises/Deli Ex4 Chap6.py
+++ Assessment 1/Chapter 6- Loops/Exercises/Deli Ex4 Chap6.py
@@ -1,11 +1,16 @@
 sandwich_orders = ['Cheese Sandwich','Bacon Sandwich', 'Grilled Cheese', 'Pastrami', 'Chicken Sandwich']
-finished_sandwiches =[]#this is a empty list
+finished_sandwiches =[]
+#this is a empty list
 
 while sandwich_orders:
-    current_sandwich = sandwich_orders.pop()#this puts all the sandwich in sandwich_orders inside current_sandwich
-    print("\nI made your",current_sandwich)#this prints out that you make the sandwich
+    current_sandwich = sandwich_orders.pop()
+#this puts all the sandwich in sandwich_orders inside current_sandwich
+    print("\nI made your",current_sandwich)
+#this prints out that you make the sandwich
     finished_sandwiches.append(current_sandwich)
+#appends puts all the sandwiches in the finished_sandwiches which was before empty
 print()
 for finished_sandwich in finished_sandwiches:
     print(finished_sandwich , " is done\n")
+#displays finished sandwiches
     
