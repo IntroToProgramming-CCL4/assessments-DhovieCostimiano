c5272b6e7961d513251b47561dfadebf66e55c13
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:18:27 +0400

    Update Your Own List Ex3 Chap3.py

--- Assessment 1/Chapter 3- Data Structures/Your Own List Ex3 Chap3.py
+++ Assessment 1/Chapter 3- Data Structures/Your Own List Ex3 Chap3.py
@@ -1,4 +1,6 @@
+#you can get certain items from the list by calling it using its list name and order number, 
 transpo = ['M113 armored personnel carrier', 'M9 Armored Combat Earthmover', 'McDonnell Douglas F-15 Eagle']
+#lists always start count from 0
 print ('I would like to own', transpo[0])
 print ('I like to ride my', transpo[2], 'to school')
 print ('I get home by riding my', transpo[1])
