5dac7fef11167706d70641f7f2ea02b858624ef3
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:21:58 +0400

    Update Stages of life Ex4 Chap4.py

--- Assessment 1/Chapter 4- Control Flow/Stages of life Ex4 Chap4.py
+++ Assessment 1/Chapter 4- Control Flow/Stages of life Ex4 Chap4.py
@@ -2,13 +2,19 @@ age = 67
 
 if age<2:
     print("That person is a baby")
+#if the person is below 2, it displays the human is a baby
 elif age>=2 and age<4:
     print("That person is a toddler")
+#if the person is below 4 and above 2, it displays the human is a toddler
 elif age>=4 and age<13:
     print("That person is a kid")
+#if the person is below 13 and above 4, it displays the human is a kid
 elif age>=13 and age<20:
     print("That person is a teenager")
+#if the person is below 20 and above 13, it displays the human is a teenager
 elif age>=20 and age<65:
     print("That person is a adult")
+#if the person is below 65 anf above 20, it displays the human is a adult
 elif age>=65:
     print("That person is  old or a senior")
+#if the person is above 65, it displays the human is a senior or is old
