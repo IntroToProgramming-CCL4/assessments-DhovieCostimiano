321f88d873ddb2eb3ff6252ee66c7cb70db70508
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:34:51 +0400

    Update Message Ex1 Chap7.py

--- Assessment 1/Chapter 7- Functions/Exercises/Message Ex1 Chap7.py
+++ Assessment 1/Chapter 7- Functions/Exercises/Message Ex1 Chap7.py
@@ -1,4 +1,5 @@
 def display_message():
     print ('In this chapter we learned about Function and how they work. \nFunctions are named blocks of code that are designed to do one specific job. \nIt maximizes code reuse and minimizes redundancy.')
 display_message()
+#a simple message function
 
