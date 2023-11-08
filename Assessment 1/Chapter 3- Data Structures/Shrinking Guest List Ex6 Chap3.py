e498dba5a5487e1bb0149dca822c9c4e2f75dc9d
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:19:49 +0400

    Update Shrinking Guest List Ex6 Chap3.py

--- Assessment 1/Chapter 3- Data Structures/Shrinking Guest List Ex6 Chap3.py
+++ Assessment 1/Chapter 3- Data Structures/Shrinking Guest List Ex6 Chap3.py
@@ -1,25 +1,34 @@
 guests = ['Janel', 'Vince', 'Danish']
 for guest in guests:
+#for loop to make it easier
     print ('I am delighted to inform that you are invited to dinner,', guest)
 
 print (guests[0], 'Cant make it to dinner')
 print ('Not coming:', guests[0])
 guests.remove ('Janel')
+#removes a guest from the list
 print ('Coming:', guests[0], guests[1])
+#displays remaining guests
 
 guests.insert (0, 'Dhovie')
+#inserts a guest in a the original list
 
 guests = ['Dhovie', 'Vince', 'Danish']
 for guest in guests:
+#for loop to make it easier
     print ('I am delighted to inform that you are invited to dinner,', guest)
 
 print ('Im really sorry i can only invite 2 people for dinner')
 poppedguest = guests.pop(0)
+#another way to remove a guest
 print ('Im sorry but you dont fit', poppedguest, 'maybe next time.')
 
 for poppedguest in guests:
+#displays the remaining guests
     print ('I am delighted to inform that you are still invited to dinner,', poppedguest)
 
 del guests [0]
+#deletes the remaining guest
 del guests [0]
+#deletes the remaining guest
 print ('Remaining guests:',guests)
