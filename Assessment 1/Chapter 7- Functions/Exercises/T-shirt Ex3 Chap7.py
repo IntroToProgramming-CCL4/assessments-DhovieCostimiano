11f64951c0b2bfe659b95da43d91f4aa55674efd
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:35:13 +0400

    Update T-shirt Ex3 Chap7.py

--- Assessment 1/Chapter 7- Functions/Exercises/T-shirt Ex3 Chap7.py
+++ Assessment 1/Chapter 7- Functions/Exercises/T-shirt Ex3 Chap7.py
@@ -1,6 +1,7 @@
 print ('Shirt sizes are:Small, Medium, Large')
 print ('You can put any message')
 def make_shirt(size, message):
+#size and message not stated so can be written by user or be in a string/variable
     print ('The size of the shirt is', size, 'and the message is', message)
 make_shirt ('Medium', 'Pogi ni Sir Oliver')
 make_shirt ('Small', 'Lalo na pag may plus points')
