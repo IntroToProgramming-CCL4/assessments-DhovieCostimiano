529d3af325651973b7cd0c684e1a149d7801eff6
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:12:17 +0400

    Update Print Date and Time Ex3 Chap1.py

--- Assessment 1/Chapter 1- Getting Started with Python/Print Date and Time Ex3 Chap1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Print Date and Time Ex3 Chap1.py
@@ -1,6 +1,8 @@
 import datetime
+#keyword to make code in one module available in another.
 now=datetime.datetime.now()
 print ('The current Date and time is:', now)
+#prints out the date and time with the use of import
 
 
 
