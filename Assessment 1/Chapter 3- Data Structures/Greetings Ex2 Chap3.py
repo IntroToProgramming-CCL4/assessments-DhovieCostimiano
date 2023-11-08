049d79f880bab919666eec7ccfa2746624f0fcc1
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:17:59 +0400

    Update Greetings Ex2 Chap3.py

--- Assessment 1/Chapter 3- Data Structures/Greetings Ex2 Chap3.py
+++ Assessment 1/Chapter 3- Data Structures/Greetings Ex2 Chap3.py
@@ -1,6 +1,8 @@
+#for loops can be used in lists
 friends = ['Danish','Vince', 'Janel']
 for friend in friends:
     print ('Wassap my very handsome and smart friend named', friend)
+#or you can print it  1 by 1 which takes longer
 print ('Wassap my very handsome and smart friend named', friends[0])
 print ('Wassap my very handsome and smart friend named', friends[1])
 print ('Wassap my very handsome and smart friend named', friends[2])
