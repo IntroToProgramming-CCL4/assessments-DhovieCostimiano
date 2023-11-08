a1d54243dc2f454957bcfc2a98915d8283ce9449
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:33:37 +0400

    Update Cities Ex5 Chap7.py

--- Assessment 1/Chapter 7- Functions/Exercises/Cities Ex5 Chap7.py
+++ Assessment 1/Chapter 7- Functions/Exercises/Cities Ex5 Chap7.py
@@ -1,5 +1,7 @@
 def describe_city(city, country='UAE'):
+#stores the city and country in a function #country by default will always be UAE Unless stated
     print (city,'is in the', country)
+#prints city and country that is stated
 describe_city ('London', 'United Kingdom')
 describe_city ('Abu Dhabi')
 describe_city ('Washington D.C.', 'USA')
