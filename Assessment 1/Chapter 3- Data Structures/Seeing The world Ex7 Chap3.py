c9f8a6734035154c9bd61273f5d687e0efb633e5
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:20:15 +0400

    Update Seeing The world Ex7 Chap3.py

--- Assessment 1/Chapter 3- Data Structures/Seeing The world Ex7 Chap3.py
+++ Assessment 1/Chapter 3- Data Structures/Seeing The world Ex7 Chap3.py
@@ -3,25 +3,31 @@ places= ['Japan','Bali','London','Spain','Monaco']
 print ("Original:", places)
 
 print ("Alphabetical:", sorted(places))
+#sorts the list alphabetically
 
 print ("Original 2:", places)
 
 print ("Reverse alphabetical:", sorted(places, reverse=True))
+#sorts the list alphabetically but in reverse
 
 print ("Original 3:", places)
 
 places.reverse()
+#reverses the list
 
 print ('Reversed:', places)
 
 places.reverse()
+#reverses the list again, so it went back to the original order
 
 print ('Original after reversing:', places)
 
 places.sort()
+#sorts list temporarily
 
 print ('Alphabetical order with Sort():', places)
 
 places.sort(reverse=True)
+#reverses the list
 
 print ('Reverse alphabetical with Sort():', places)
