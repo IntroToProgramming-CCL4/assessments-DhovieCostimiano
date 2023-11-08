7ea51b41b6186800eca78ec55390b2ec729a75b5
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:16:02 +0400

    Update Stripping Names Ex3 Chap2.py

--- Assessment 1/Chapter 2- Variables & Comments/Stripping Names Ex3 Chap2.py
+++ Assessment 1/Chapter 2- Variables & Comments/Stripping Names Ex3 Chap2.py
@@ -1,7 +1,10 @@
 name = '\t\n\tDhovie Bryan Costimiano\t'
 stripped_nameL = name.lstrip()
+#Removes left most space
 stripped_nameR = name.rstrip()
+#Removes right most sapce
 stripped_nameS= name.strip()
+#Removes both spaces
 print (name)
 print (stripped_nameL)
 print (stripped_nameR)
