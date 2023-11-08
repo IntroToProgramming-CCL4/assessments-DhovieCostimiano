e32613c68f7db66a881d9c16a1b64ea50e8ad0c9
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:13:00 +0400

    Update Strings Concatination Ex4 Chap1.py

--- Assessment 1/Chapter 1- Getting Started with Python/Strings Concatination Ex4 Chap1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Strings Concatination Ex4 Chap1.py
@@ -1,4 +1,6 @@
+#A string is a collection of alphabets, words or other characters
 string1 = "Dhovie Bryan"
 string2 = " Romina"
 string3 = " Costimiano"
 print (string1+string2+string3)
+#strings can be added together using the + symbol
