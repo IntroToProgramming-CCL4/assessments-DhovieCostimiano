aa6369838928879ad0676f4fc4633678ca24b2af
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:21:37 +0400

    Update Favorite Friut Ex5 Chap4.py

--- Assessment 1/Chapter 4- Control Flow/Favorite Friut Ex5 Chap4.py
+++ Assessment 1/Chapter 4- Control Flow/Favorite Friut Ex5 Chap4.py
@@ -1,5 +1,13 @@
 favorite_fruits=['banana','guava','mandarin','dragon fruit','grapes']
-for fruits in favorite_fruits:
-    print ('You really like', fruits)
-
-    
+#5 if statements
+#if the word 'banana' is in favorite fruit it will print out "you really like bananas!"
+if 'banana' in favorite_fruits:
+    print('You really like bananas!')
+if 'guava' in favorite_fruits:
+    print('You really like guava!')
+if 'mandarin' in favorite_fruits:
+    print('You really like mandarin!')
+if 'dragon fruit' in favorite_fruits:
+    print('You really like dragon fruit!')
+if 'grapes' in favorite_fruits:
+    print('You really like grapes!')
