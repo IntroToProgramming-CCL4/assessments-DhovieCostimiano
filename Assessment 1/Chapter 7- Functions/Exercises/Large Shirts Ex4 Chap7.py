e47c8e509304a298957750b0d621e09895c5aad6
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:34:33 +0400

    Update Large Shirts Ex4 Chap7.py

--- Assessment 1/Chapter 7- Functions/Exercises/Large Shirts Ex4 Chap7.py
+++ Assessment 1/Chapter 7- Functions/Exercises/Large Shirts Ex4 Chap7.py
@@ -1,4 +1,5 @@
 def make_shirt(size='Large', message='I love Python'):
+#size and message are already given if not over written will go back to this by defaut
     print ('The size of the shirt is', size, 'and the message is', message)
 make_shirt ('"Medium"', '"Pogi ni Sir Oliver"')
 make_shirt ('"Small"', '"Lalo na pag may plus points"')
