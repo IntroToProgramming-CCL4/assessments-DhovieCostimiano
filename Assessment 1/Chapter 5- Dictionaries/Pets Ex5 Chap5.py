358287f6947e146f10e821b2f36968912f24d81d
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:25:43 +0400

    Update Pets Ex5 Chap5.py

--- Assessment 1/Chapter 5- Dictionaries/Pets Ex5 Chap5.py
+++ Assessment 1/Chapter 5- Dictionaries/Pets Ex5 Chap5.py
@@ -4,5 +4,7 @@ owner3= {'Pet name':'Zeus', 'Owner name': 'Bryan'}
 owner4= {'Pet name':'Skoby', 'Owner name': 'Yanna'}
 owner5= {'Pet name':'Nido', 'Owner name': 'Racelin'}
 pets=[owner1, owner2, owner3, owner4, owner5]
+#you can combine all dictionaries in a list
 for owner in pets:
+#you can print all the dictionaries by calling out only the list
     print (owner)
