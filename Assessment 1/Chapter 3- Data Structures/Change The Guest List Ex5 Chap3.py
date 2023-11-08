97fbbe6f3ba93262a5a391ad93d714d4df7a9c5d
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:19:23 +0400

    Update Change The Guest List Ex5 Chap3.py

--- Assessment 1/Chapter 3- Data Structures/Change The Guest List Ex5 Chap3.py
+++ Assessment 1/Chapter 3- Data Structures/Change The Guest List Ex5 Chap3.py
@@ -1,14 +1,21 @@
 guests = ['Janel', 'Vince', 'Danish']
+#list
 for guest in guests:
+#you can use for loops in a list
     print ('I am delighted to inform that you are invited to dinner,', guest)
+#For loops can shorten a long code
 
 print (guests[0], 'Cant make it to dinner')
 print ('Not coming:', guests[0])
 guests.remove ('Janel')
+#removes guest from the list
 print ('Coming:', guests[0], guests[1])
+#displays remaining guests
 
 guests.insert (0, 'Dhovie')
+#inserts guest in the list
 
 guests = ['Dhovie', 'Vince', 'Danish']
 for guest in guests:
     print ('I am delighted to inform that you are invited to dinner,', guest)
+#for loop again to make it shorter
