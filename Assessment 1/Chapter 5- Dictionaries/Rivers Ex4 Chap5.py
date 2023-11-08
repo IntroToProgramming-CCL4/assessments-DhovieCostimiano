83342935930853db2a2259989e069104009706e4
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:29:58 +0400

    Update Rivers Ex4 Chap5.py

--- Assessment 1/Chapter 5- Dictionaries/Rivers Ex4 Chap5.py
+++ Assessment 1/Chapter 5- Dictionaries/Rivers Ex4 Chap5.py
@@ -1,7 +1,13 @@
 rivers = {'Nile River': 'Egypt', 'Amazon River': 'South America', 'Yangtze River': 'China'}
 for key, value in rivers.items():
+#Calls both key and value
     print ('\nThe', key, 'is located in', value)
+#displays both values and keys
 for key in rivers.keys():
+#calls only keys
     print ('\nRiver name:', key)
+#displays only keys
 for value in rivers.values():
+#call only values
     print ('\nlocation:', value)
+#displays only values
