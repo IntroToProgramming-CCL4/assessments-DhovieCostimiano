3ac37f95b0852b52c3adc3065942c2f5e9d87979
DhovieCostimiano <144931256+DhovieCostimiano@users.noreply.github.com>
2023-11-08 19:32:39 +0400

    Update Pizza Toppings Ex1 Chap6.py

--- Assessment 1/Chapter 6- Loops/Exercises/Pizza Toppings Ex1 Chap6.py
+++ Assessment 1/Chapter 6- Loops/Exercises/Pizza Toppings Ex1 Chap6.py
@@ -1,6 +1,8 @@
 toppings= input('What topping would you like to add:')
 while toppings != 'quit':
+    #if user input "quit" the code stops
     print ('I will add', toppings, 'to your pizza!')
     toppings = input ('would you like to add more?:')
+    #keeps asking user to input a topping
 else:
     print ('Thank you, have a great day.')
